9b0527154627367db179da33b2c88e653439444f
BliznyukT <bliznyuk10@mail.ru>
2025-08-28 04:50:13 +0300

    Second commit in new-feature branch. Add method newFunction() in BaseLogic class

--- src/mainbranchclases/BaseLogic.java
+++ src/mainbranchclases/BaseLogic.java
@@ -4,4 +4,8 @@ public class BaseLogic {
     public static void main(String[] args) {
         System.out.println("Класс из main ветки");
     }
+
+    public static void newFunction() {
+        System.out.println("This function has been added in new-feature branch");
+    }
 }
