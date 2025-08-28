0a4a3b653c8725f499b5832c1bf8bcc51b7b899e
BliznyukT <bliznyuk10@mail.ru>
2025-08-28 04:53:03 +0300

    Third commit in main branch, mainFunction() has been added

--- src/mainbranchclases/BaseLogic.java
+++ src/mainbranchclases/BaseLogic.java
@@ -4,4 +4,8 @@ public class BaseLogic {
     public static void main(String[] args) {
         System.out.println("Класс из main ветки");
     }
+
+    public static void mainFunction() {
+        System.out.println("Important function has been added in main branch");
+    }
 }
